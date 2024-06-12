222ef47fe19b8e5c811f80fbaa50585af639178d
AceLongli <167272284+AceLongli@users.noreply.github.com>
2024-06-12 15:57:02 +0800

    Add files via upload

---
+++ 02/07.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+ 
+app = Flask(__name__)
+ 
+img = os.path.join('static', 'photo')
+ 
+@app.route('/')
+def home():
+    file = os.path.join(img, '1.gif')
+    return render_template('index.html', image=file)
+ 
+if __name__ == '__main__':
+    app.run()
